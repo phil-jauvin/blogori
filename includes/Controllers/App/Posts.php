22e9362246b1edb2706449b9752b5c7c77fa547e
phil-jauvin <philippejauvin@gmail.com>
2017-06-26 22:34:24 +0100

    More restfulness

--- includes/Controllers/App/Posts.php
+++ includes/Controllers/App/Posts.php
@@ -7,34 +7,32 @@ use \Sosmol\Models\Post;
 class Posts {
 
 	public function Post( $id ){
+
 		if( $_SERVER['REQUEST_METHOD'] === 'GET' ){
 			$post = new Post( $id );
 			var_dump( $post );
 		}
-	}
 
-  public function CreatePost(){
-		if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
-	    $post = new Post();
-			$post->Create( $_POST['title'], $_POST['category'], $_POST['content'], $_POST['basename'] );
-		}
-    //$post->Create( 'test post', 'uncategorised', 'test content', 'test-post' );
-  }
-
-  public function UpdatePost( $id ){
 		if( $_SERVER['REQUEST_METHOD'] === 'PUT' ){
 			$request = file_get_contents('php://input');
 			parse_str($request, $params);
 			$post = new Post( $id );
 			$post->Update( $params['title'], $params['category'], $params['content'], $params['basename'] );
 		}
-  }
 
-  public function DeletePost( $id ){
 		if( $_SERVER['REQUEST_METHOD'] === 'DELETE' ){
 			$post = new Post( $id );
 			$post->Delete();
 		}
-  }
+
+	}
+
+	public function CreatePost(){
+		if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+			$post = new Post();
+			$post->Create( $_POST['title'], $_POST['category'], $_POST['content'], $_POST['basename'] );
+		}
+		//$post->Create( 'test post', 'uncategorised', 'test content', 'test-post' );
+	}
 
 }
