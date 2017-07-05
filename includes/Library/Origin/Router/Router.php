c31813acceb61831b9fb42f801db14985ed7e5fc
phil-jauvin <philippejauvin@gmail.com>
2017-07-05 22:29:41 +0100

    remove mandatory routes in router

--- includes/Library/Origin/Router/Router.php
+++ includes/Library/Origin/Router/Router.php
@@ -18,7 +18,7 @@ class Router extends \Origin\Utilities\Types\Singleton {
   const CONTROLLER_BASE_NAMESPACE = '\\Controllers';
   const DIR = '/';
 	const NS = '\\';
-  
+
   /*
   * Called from any file you wish to do routing from. Usually only index.php in the public directory.
   * It is possible to specify a specific route via string as well (EG: For redirects in old code paths and the like).
@@ -28,12 +28,12 @@ class Router extends \Origin\Utilities\Types\Singleton {
 		if(empty($route)){
 			return $this->Route('*');
 		}
-    
+
     // Exact match.
     if($this->routes->offsetExists($route)){
       return $this->PathFinder($this->routes->offsetGet($route), $route);
     }
-    
+
     // Regex match. I'm sure someone who's brighter than I am has a better solution for this in 30 lines or less.
 		foreach($this->routes as $pattern => $path){
 			if(@preg_match($this->RegexifyPattern($pattern), null) !== false){
@@ -43,10 +43,10 @@ class Router extends \Origin\Utilities\Types\Singleton {
 				}
 			}
 		}
-		
-		return $this->Route('404');
+
+		return $this->Route('mainview');
   }
-  
+
   /*
   * Configuration and private functions.
   */
@@ -56,16 +56,8 @@ class Router extends \Origin\Utilities\Types\Singleton {
   public function __construct(){
     $this->routes = Settings::Get('routes')->Values(['routes']);
     $this->max_route_attempts = Settings::Get()->Value(['origin', 'max_route_attempts']);
-		
-		if(!$this->routes->offsetExists('*')){
-      throw new Exception('You must specify a default route in your routes file.');
-    }
-    
-    if(!$this->routes->offsetExists('404')){
-      throw new Exception('You must specify a 404 route in your routes file.');
-    }
   }
-  
+
   /*
   * At this point we know the route we're going to use. Now to break it apart and call it.
   */
@@ -75,16 +67,16 @@ class Router extends \Origin\Utilities\Types\Singleton {
         $class = $this->GetClass($path);
 				Layout::Get()->Assign('route', $route);
 				if(call_user_func_array(array((new $class), $this->GetMethod($path)), $variables) === false){
-          throw new Exception('File exists, but class is undefined for route: '.$this->GetClass($path).'->'.$this->GetMethod($path)); 
+          throw new Exception('File exists, but class is undefined for route: '.$this->GetClass($path).'->'.$this->GetMethod($path));
         }
-        
+
         return true;
       }
-      
+
       return $this->Route('404');
     }
   }
-  
+
   /*
   * Finds the file for our route based on $path. If the file exists we'll require it.
   */
@@ -94,10 +86,10 @@ class Router extends \Origin\Utilities\Types\Singleton {
       require_once(sprintf(self::CONTROLLER_BASE_PATH, implode(self::DIR, $path)));
       return true;
     }
-    
+
     return false;
   }
-  
+
   /*
   * Returns the full class (including namespace) based on $path.
   */
@@ -105,7 +97,7 @@ class Router extends \Origin\Utilities\Types\Singleton {
     $path = $this->ClassFileCommon($path);
     return self::CONTROLLER_BASE_NAMESPACE.self::NS.implode(self::NS, $path);
   }
-  
+
   /*
   * Shared code between GetFile() and GetClass(). Returns the namespaces and class as an array while stripping off the method.
   */
@@ -113,7 +105,7 @@ class Router extends \Origin\Utilities\Types\Singleton {
     $path = explode(self::DIR, $path);
     return array_slice($path, 0, (count($path) - 1));
   }
-  
+
   /*
   * Retrieves the method name from $path.
   */
@@ -121,9 +113,9 @@ class Router extends \Origin\Utilities\Types\Singleton {
     $path = explode(self::DIR, $path);
     return $path[(count($path) - 1)];
   }
-  
+
   /*
-  * We'd only like to attempt to route a maximum number of times (otherwise we're likely in an infinite loop). 
+  * We'd only like to attempt to route a maximum number of times (otherwise we're likely in an infinite loop).
   * This increments the counter and stops the madness in event of "failure".
   * This shouldn't be hit under normal circumstances unless another file makes a call to route itself or something rediculous.
   */
@@ -132,10 +124,10 @@ class Router extends \Origin\Utilities\Types\Singleton {
     if($this->attempts > $this->max_route_attempts){
       throw new Exception('Routing has tried multiple times to find the correct path, but has failed. This likely means your class does not exist or is not in the correct location/namespace.');
     }
-    
+
     return true;
   }
-	
+
 	private static $find = array('/', '*');
 	private static $replace = array('\/', '\*');
 	private function RegexifyPattern($pattern){
